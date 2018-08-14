f758bc670f292b9ab34f3e1ca3609b19b72b3714
Tyler Collier <tyler.collier+github@devmounta.in>
2018-08-13 23:45:03 -0600

    Step 8

--- server/server.js
+++ server/server.js
@@ -16,6 +16,19 @@ app.use(
     saveUninitialized: false
   })
 );
+app.use((req, res, next) => {
+  let badWords = ['knucklehead', 'jerk', 'internet explorer'];
+  if (req.body.message) {
+    let badWordsExist = true;
+    for (let i = 0; i < badWords.length; i++) {
+      let regex = new RegExp(badWords[i], 'g');
+      req.body.message = req.body.message.replace(regex, '****');
+    }
+    next();
+  } else {
+    next();
+  }
+});
 
 app.get('/api/messages', messagesCtrl.getAllMessages);
 app.get('/api/messages/history', messagesCtrl.history);
