a41fb163a30e4f2d16c57330abe543a2c0c18a96
Tyler Collier <tyler.collier+github@devmounta.in>
2018-08-13 23:33:59 -0600

    Step 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import HistoryModal from './components/HistoryModal';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
@@ -13,6 +14,21 @@ class App extends Component {
       showHistory: false
     };
     this.closeHistoryModal = this.closeHistoryModal.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get('/api/messages').then(res => {
+      this.setState({ allMessages: res.data });
+    });
+  }
+
+  createMessage() {
+    let { username, message } = this.state;
+    axios
+      .post('/api/messages', { username: username, message: message })
+      .then(res => {
+        this.setState({ allMessages: res.data });
+      });
   }
 
   saveUsername() {
