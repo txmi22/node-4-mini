086f1dc3704c072f34ea407a4ba2b42a4e074b5d
Tyler Collier <tyler.collier+github@devmounta.in>
2018-08-13 23:36:11 -0600

    Step 5

--- server/server.js
+++ server/server.js
@@ -2,12 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const messagesCtrl = require('./messagesCtrl');
+const session = require('express-session');
 
-let { SERVER_PORT } = process.env;
+let { SERVER_PORT, SESSION_SECRET } = process.env;
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 app.get('/api/messages', messagesCtrl.getAllMessages);
 app.post('/api/messages', messagesCtrl.createMessage);
